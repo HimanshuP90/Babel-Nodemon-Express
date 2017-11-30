8bc78fe18314dd2b7ce9c43f96cd21421ab3aec8
HimanshuP90 <himanshup895@gmail.com>
2017-11-30 22:54:31 +0530

    Added App page

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Home';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<Home />
+			</div>
+		)
+	}
+}
+
+export default App;
